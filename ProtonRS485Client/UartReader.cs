f33d19b2f802d4b2ab33d2bbd4a3c3358aeb9655
Andrey Cherepanov <vortexpiral@gmail.com>
2017-08-16 21:26:35 +0400

    Refactoring

--- ProtonRS485Client/UartReader.cs
+++ ProtonRS485Client/UartReader.cs
@@ -20,50 +20,6 @@ namespace ProtonRS485Client
         public bool Connected { get; set; }
         public byte FrameLength { get; set; }
         public byte[] Data { get; set; }
-        public byte DataHandle { get; set; }
-
-        private void UpdateState()
-        {
-            
-        }
-        private const byte DataHandleInitialValue = 2;
-        public void InitializeDataArray()
-        {
-            Data = new byte[FrameLength];
-            Data[0] = SlaveAddress;
-            Data[1] = FrameLength;
-            DataHandle = DataHandleInitialValue; 
-        }
-
-        public bool Read(byte input)
-        {
-            if (DataHandle < FrameLength)
-            {
-                Data[DataHandle++] = input;
-                return false;
-            }
-            if (UartHelper.GetCrc(Data, 0, FrameLength) == input)
-            {
-                //Log.LogWrite("receive: " + BitConverter.ToString(_data).Replace("-", " "));
-                //SendData(serialPort, _commandLevel.ProcessCommand(_data));  
-            }
-            return true;
-        }
-
-        public bool IsLengthInRange(byte length)
-        {
-            return length >= 4 && length <= 14;
-        }
-
-        public bool SetFrameLength(byte length)
-        {
-            FrameLength = length;
-            if (!IsLengthInRange(FrameLength))
-            {
-                return false;
-            }
-            InitializeDataArray();
-            return true;
-        }
+        public byte DataHandle { get; set; }  
     }
 }
--- ProtonRS485Client/UartReaderDataDispatcher.cs
+++ ProtonRS485Client/UartReaderDataDispatcher.cs
@@ -7,6 +7,44 @@ using System.Threading.Tasks;
 namespace ProtonRS485Client
 {
     class UartReaderDataDispatcher
-    {
+    {       
+        private const byte DataHandleInitialValue = 2;
+        public void InitializeDataArray()
+        {
+            Data = new byte[FrameLength];
+            Data[0] = SlaveAddress;
+            Data[1] = FrameLength;
+            DataHandle = DataHandleInitialValue;
+        }
+
+        public bool IsLengthInRange(byte length)
+        {
+            return length >= 4 && length <= 14;
+        }
+
+        public bool SetFrameLength(byte length)
+        {
+            FrameLength = length;
+            if (!IsLengthInRange(FrameLength))
+            {
+                return false;
+            }
+            InitializeDataArray();
+            return true;
+        }
+        public bool Read(byte input)
+        {
+            if (DataHandle < FrameLength)
+            {
+                Data[DataHandle++] = input;
+                return false;
+            }
+            if (UartHelper.GetCrc(Data, 0, FrameLength) == input)
+            {
+                //Log.LogWrite("receive: " + BitConverter.ToString(_data).Replace("-", " "));
+                //SendData(serialPort, _commandLevel.ProcessCommand(_data));  
+            }
+            return true;
+        }
     }
 }
